0116e165a6146c3c2bb813535b7cec040420d4af
Taen Ahammed <taenahammed7@gmail.com>
2019-07-08 11:48:19 +0600

    Assignment - 3: Event Listeners added.

---
+++ Assignment-3/script.js
@@ -0,0 +1,66 @@
+class Calculator {
+    addition() {}
+    subtraction() {}
+    multiplication() {}
+    division() {}
+}
+
+document
+    .getElementById("zero")
+    .addEventListener("click", () => console.log("zero"));
+
+document
+    .getElementById("one")
+    .addEventListener("click", () => console.log("one"));
+
+document
+    .getElementById("two")
+    .addEventListener("click", () => console.log("two"));
+
+document
+    .getElementById("three")
+    .addEventListener("click", () => console.log("three"));
+
+document
+    .getElementById("four")
+    .addEventListener("click", () => console.log("four"));
+
+document
+    .getElementById("five")
+    .addEventListener("click", () => console.log("five"));
+
+document
+    .getElementById("six")
+    .addEventListener("click", () => console.log("six"));
+
+document
+    .getElementById("seven")
+    .addEventListener("click", () => console.log("seven"));
+
+document
+    .getElementById("eight")
+    .addEventListener("click", () => console.log("eight"));
+
+document
+    .getElementById("nine")
+    .addEventListener("click", () => console.log("nine"));
+
+document
+    .getElementById("plus")
+    .addEventListener("click", () => console.log("plus"));
+
+document
+    .getElementById("minus")
+    .addEventListener("click", () => console.log("minus"));
+
+document
+    .getElementById("mul")
+    .addEventListener("click", () => console.log("mul"));
+
+document
+    .getElementById("div")
+    .addEventListener("click", () => console.log("div"));
+
+document
+    .getElementById("equal")
+    .addEventListener("click", () => console.log("equal"));
